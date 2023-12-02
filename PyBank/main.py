c1a0303624e956e81028cb99946ce0f5c52d8fd0
Vkrishm <149877083+Vkrishm@users.noreply.github.com>
2023-12-03 10:27:52 +1100

    first push

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,7 @@ csv_path = os.path.join('D:/Data Boot Camp/Module 3 challenge/Python-challenge/P
 # CSV headers: Date, Profit/Losses
 date = [0]
 Profit_losses = [1]
-
+count_months = 0
 
 # 'Resources', 'budget_data.csv'
 
@@ -16,9 +16,17 @@ Profit_losses = [1]
 # Read in the CSV file
 with open(csv_path) as csv_file:
 
-    #Split the data on commas
+    # CSV reader specifies delimiter and variable that holds contents
     csv_reader = csv.reader(csv_file, delimiter=',')
+
     # Read the header row first
     csv_header = next(csv_reader)
+    # Print header row from CSV
+    print(f"CSV Header: {csv_header}")
 
-    print(csv_reader)
+    # Read each row of data after the header
+    for row in csv_reader:
+    
+    # Count number of months
+    count_months += 1
+
